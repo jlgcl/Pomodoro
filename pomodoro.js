c493ead5cc05e9b9bd816c8ed84926084fa4c800
James <jwljacl@gmail.com>
2019-12-11 23:45:25 -0500

    rev2

--- pomodoro.js
+++ pomodoro.js
@@ -14,7 +14,7 @@ LEARNED:
 
 STATUS:
     - update display time depending on workDisp input - COMPLETED
-    - countdown from existing disp value.
+    - countdown from existing disp value. - COMPLETED
     - start/stop/refresh
     - after disp value is done, replace the value with rest disp.
 
@@ -62,34 +62,45 @@ function setClock() {
         restDisp.textContent = timeRd - 1;
     })
 
+
+    let timeB = [];
+
     start.addEventListener("click", function() {
         
         setInterval(function() {
         
             let timeA = disp.textContent.split("");                //['2', '5', ':', '0', '0']; ['2', '5', ':', '5', '9'] NOT: ['2', '5', ':', '59']
-            //STATUS BOOKMARK: splitting in to 4 arrays again; we must make it so that we are passing "59", not "5", "9"
-            var timeB = timeA.map(function(x){                     //[2, 5, NaN, 0, 0]
+            //STATUS BOOKMARK1 (COMPLETED): splitting in to 4 arrays again; we must make it so that we are passing "59", not "5", "9"
+            let timeJoin = timeA.join("").split(":");
+            timeJoin.splice(1, 0, ":");                            //["25", ":", "59"]
+            
+            timeB = timeJoin.map(function(x){                     //[25, NaN, 00]
                 return parseInt(x);
             })                          
-            timeB.splice(2, 1, ":");                                //[2, 5, ":", 0, 0]
-            timeB.splice(4, 1);                                     //[2, 5, ":", 0]
+            timeB.splice(1, 1, ":");                                //[25, ":", 00] 
             console.log(timeB)
         
-            if (timeB[3] == 0) {
-                timeB[3] = 59;
-                timeB.splice(3, 1, timeB[3]);                       //[2, 5, ":", 59]
+            if (timeB[2] == 0) {
+                timeB[2] = 59;
+                timeB[0] = timeB[0] - 1;
+                timeB.splice(2, 1, timeB[2]);                       //[25, ":", 59]
                 disp.textContent = timeB.join("");                  //[25:59]
                 //display must be in form: "xx:xx" in string
-                console.log(timeB);
-                
+                //console.log(timeB);
             }else {
-                timeB.splice(3, 1, (timeB[3]-1));                   //why does this only increment third digit? 59 -> 4 -> 3 -> ...
+                timeB.splice(2, 1, (timeB[2]-1));                   //why does this only increment third digit? 59 -> 4 -> 3 -> ...
                 disp.textContent = timeB.join("");
-                console.log(timeB);
+                //console.log(timeB);
             }
 
         },1000)
     })
+
+    pause.addEventListener("click", function() {
+        disp.textContent = timeB.join("");
+        //STATUS BOOKMARK2: pause the display.
+    })
+
 }
 
 setClock();
